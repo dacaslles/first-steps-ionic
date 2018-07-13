6bbec5613f4527da12eec5b3a58d44f56fa64d5e
dacaslles <dacaslles@gmail.com>
2018-07-13 15:03:47 +0000

    dynamic nav and grid added

--- MyFirstApp/src/pages/contact/contact.ts
+++ MyFirstApp/src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MasinfoPage} from '../masinfo/masinfo';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +8,28 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+  list = [];
+  
   constructor(public navCtrl: NavController) {
+    this.list = [
+      {
+        'title': 'Angular',
+        'icon': 'angular',
+        'description': 'Angular description',
+        'color': '#E63135'
+      },
+      {
+        'title': 'CSS3',
+        'icon': 'css3',
+        'description': 'CSS3 description',
+        'color': '#0CA9EA'
+      }
 
+    ];
+  }
+
+  openNavDetailsPage(item){
+    this.navCtrl.push(MasinfoPage, {item: item});
   }
 
 }
--- MyFirstApp/src/pages/masinfo/masinfo.ts
+++ MyFirstApp/src/pages/masinfo/masinfo.ts
@@ -15,7 +15,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MasinfoPage {
 
+  item;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.item = navParams.data.item;
   }
 
   ionViewDidLoad() {
